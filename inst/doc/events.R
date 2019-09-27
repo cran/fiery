361f88254e4c7611def63f152bfe6d239b2b5103
Thomas Lin Pedersen <thomasp85@gmail.com>
2019-09-27 05:50:03 +0000

    version 1.1.2

---
+++ inst/doc/events.R
@@ -0,0 +1,18 @@
+## ---- eval=FALSE---------------------------------------------------------
+#  # Add a handler to the 'new-event' event
+#  id <- app$on('new-event', function() {
+#    message('Event fired')
+#  })
+#  
+#  # Trigger the event
+#  app$trigger('new-event')
+#  
+#  # Remove the handler
+#  app$off(id)
+
+## ---- eval=FALSE---------------------------------------------------------
+#  app$trigger_dir <- '/some/path/to/dir/'
+
+## ---- eval=FALSE---------------------------------------------------------
+#  saveRDS(list(arg1 = 'test'), '/some/path/to/dir/external-event.rds')
+
